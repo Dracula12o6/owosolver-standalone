eae653cb361c05912bcd2d4b6201bba0286cd9a9
drac <139640096+Dracula12o6@users.noreply.github.com>
2026-06-01 18:28:30 +0530

    Refine comments in config.js for proxy settings
    Updated comments for clarity regarding proxy usage.

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
     SOLVER_CONFIG: {
         sitekey: 'a6a1d5ce-612d-472d-8e37-7601408fbc09', // owo hcaptcha sitekey
         baseurl: 'http://owobot.com/',  // owo captcha url
-        proxy: '',  // Leave empty if no proxy
-        ApiKey: 'YOUR_API_KEY_HERE', // Scarnox ApiKey , 0.6$/1k with ur own proxy
-        endpoint: 'https://api.scarnox.com/api/tasks/create'                                   // or 0.75$/1k proxyless
+        proxy: '',  // Leave empty if no proxy depending on your configuration
+        ApiKey: 'YOUR_API_KEY_HERE', // Scarnox ApiKey , 0.6$/1k with ur own proxy or 0.75$/1k proxyless
+        endpoint: 'https://api.scarnox.com/api/tasks/create'                                  
     }
 };
